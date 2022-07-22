5afd2f05efd68c396c5438d4efb4921b8f9fb32f
ElfDwarf <elf.or.dwarf@gmail.com>
2022-07-22 13:44:38 +0500

    Initialize with spring initializr

---
+++ src/main/java/com/example/poolregistration/PoolResgistrationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.poolregistration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoolResgistrationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoolResgistrationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/poolregistration/PoolResgistrationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.poolregistration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoolResgistrationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
